c4350f5551b3c43d25632c6be06eba7c12267db5
superpaulza <18383834+superpaulza@users.noreply.github.com>
2021-10-12 01:21:18 +0700

    clean a lot of codes!

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+//import essentials library
+import 'dart:async';
+import 'dart:io';
+
+//import widget library
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+//import routes
+import 'package:score_scanner/pages/camera.dart';
+import 'package:score_scanner/pages/home.dart';
+import 'package:score_scanner/pages/second.dart';
+import 'package:score_scanner/pages/display.dart';
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(MaterialApp(
+      //Setting
+      title: 'Score Scanner Mobile Application',
+      theme: ThemeData.dark(),
+      //Routing
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => new HomePage(),
+        '/2': (BuildContext context) => new SecondPage(),
+        '/cam': (BuildContext context) => new TakePictureScreen(camera: firstCamera),
+        '/dis': (BuildContext context) => new DisplayPictureScreen(imagePath: argument[])
+      })
+    );
+}
+
+
+class Arguments {
+  final String title_bar;
+  final String text_message;
+ 
+  Arguments(this.title_bar, this.text_message);
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# score_scanner
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## Useful Resource
+Connecting Python Backend to Flutter Backend:
+- [the python code in the Flutter app?](https://smazee.com/blog/how-to-run-the-python-code-in-the-flutter-app)
+
+Designing or Mockup the Flutter UI without coding:
+- [FlutterFlow](https://app.flutterflow.io/)
+
